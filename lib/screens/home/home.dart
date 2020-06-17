5abc3711032993f3798a745e4c8140558830043e
Gimhana-Dissanayake <gimhanadev@gmail.com>
2020-06-17 19:22:55 +0530

    Initial commit

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,52 @@
+import 'package:brewcrew/models/brew.dart';
+import 'package:brewcrew/screens/home/brew_list.dart';
+import 'package:brewcrew/screens/home/settings_form.dart';
+import 'package:brewcrew/services/auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:brewcrew/services/database.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    void _showSettingPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: SettingsForm(),
+            );
+          });
+    }
+
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+            FlatButton.icon(
+                onPressed: () => _showSettingPanel(),
+                icon: Icon(Icons.settings),
+                label: Text('settings'))
+          ],
+        ),
+        body: BrewList(),
+      ),
+    );
+  }
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,69 @@
+import 'package:brewcrew/models/user.dart';
+import 'package:brewcrew/services/database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user obj based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  //auth change user stream
+  Stream<User> get user{
+//    return _auth.onAuthStateChanged.map((FirebaseUser user)=> _userFromFirebaseUser(user));
+
+  return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
+
+  //sign in in anomyn
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+//sign in with email & password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try{
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+
+//register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try{
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+
+      //create a new document from the user with the uid
+      await DatabaseService(uid:user.uid).updateUserData('0', 'new crew memeber', 100);
+      return _userFromFirebaseUser(user);
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+//sign out
+Future signOut() async{
+    try{
+      return await _auth.signOut();
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+}
+}
